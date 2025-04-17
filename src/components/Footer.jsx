56f74361a7c5d735c1b01795606edd482f9249a9
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-17 23:34:25 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="mt-8 py-4 border-t border-gray-300 w-full text-center text-gray-500 text-sm">
-        @Shreyansh, AI Image Enhancer. All rights reserved.
+        @Shreyansh0009, AI Image Enhancer. All rights reserved.
     </footer>
   )
 }
