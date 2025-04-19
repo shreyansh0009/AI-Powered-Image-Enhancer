e1cc67e9cf9aca5db789ef8ecbd0ae71a6aba5b0
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-19 18:33:01 +0530

    updated

--- src/utils/enhancedImageapi.js
+++ src/utils/enhancedImageapi.js
@@ -6,10 +6,10 @@ const API_URL = "https://techhk.aoscdn.com/";
 export const enhancedImageURL = async (file) => {
   try {
     const taskId = await uploadImage(file);
-    console.log("Image uploaded successfully, task ID:", taskId);
+   // console.log("Image uploaded successfully, task ID:", taskId);
 
     const enhancedImage = await poolEnhancedImage(taskId);
-    console.log("Image enhanced successfully, URL:", enhancedImage);
+   // console.log("Image enhanced successfully, URL:", enhancedImage);
 
     return enhancedImage;
   } catch (error) {
@@ -36,7 +36,7 @@ const uploadImage = async (file) => {
     throw new Error("Failed to upload image! Task ID not found.");
   }
 
-  console.log("Upload response:", data);
+ // console.log("Upload response:", data);
 
   return data.data.task_id;
 };
@@ -51,7 +51,7 @@ const getEnhancedImage = async (taskId) => {
     }
   );
 
-  console.log("API Response:", data); //
+ // console.log("API Response:", data); //
   if (!data?.data) {
     throw new Error("Invalid API response");
   }
